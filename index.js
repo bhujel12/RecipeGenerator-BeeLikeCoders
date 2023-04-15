c90e1f08b8fc62525a9f7312595af9d007eebad8
bhujel12 <71912832+bhujel12@users.noreply.github.com>
2023-04-15 16:56:53 -0700

    Can Search Dynamic Ingredients

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
 
-const form = document.querySelector('#ingredientForm');
+const ingredientForm = document.querySelector('#ingredientForm');
+const submitForm = document.querySelector('#submitForm');
 const input = document.querySelector('#ingredientInput');
 const addIngredientButton = document.querySelector('#addIngredientButton');
 const recipesList = document.querySelector('#recipesList');
-const recipeArray = [];
+let recipeArray = [];
 const lis = document.querySelectorAll('li');
 
-
 recipesList.addEventListener('click', function (e) {
+    recipeArray.splice(recipeArray.indexOf(e.target.innerText), 1);
     e.target.remove();
-})
+});
 
-form.addEventListener('submit', (e) => {
+ingredientForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const ingredientName = input.value;
     const newIngredient = document.createElement('li');
     newIngredient.innerText = ingredientName;
     recipesList.append(newIngredient);
+    recipeArray.push(ingredientName);
     input.value = '';
 });
+const APIKey = "f6950064452e44f59f268d1c1f420aa9";
 
-// formSubmit.addEventListener('submit', async function (event) {
-//     event.preventDefault();
-//     const query = input.value;
-//     const configuration = { params: { q: query, key: APIKey } }
-//     // We could also just use the built in fetch function honestly
-//     axios.get(`https://api.spoonacular.com/recipes/complexSearch`, configuration)
-//         .then(function (response) {
-//             addLocationToScreen(response.data);
-//         })
-//         .catch(function (error) {
-//             console.log(error);
-//         })
-// });
+submitForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    let recipeUrlInput = "";
+    for (let i = 0; i < recipeArray.length; i++) {
+        recipeUrlInput += recipeArray[i].toString() + ",+";
+    }
+    console.log(recipeUrlInput);
+    axios.get("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + recipeUrlInput + "&apiKey=" + APIKey + "&includeNutrition=true.")
+
+        .then(function (response) {
+            console.log(response)
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+});
 
