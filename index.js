1976373ced0226c53b2fbe17d6563f3a716dec1e
bhujel12 <71912832+bhujel12@users.noreply.github.com>
2023-04-15 18:23:14 -0700

    Get Images And Post on Page

--- index.js
+++ index.js
@@ -31,12 +31,63 @@ submitForm.addEventListener('submit', async function (event) {
     }
     console.log(recipeUrlInput);
     axios.get("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + recipeUrlInput + "&apiKey=" + APIKey + "&includeNutrition=true.")
-
         .then(function (response) {
             console.log(response)
+            removeOldImages();
+            makeImages(response.data)
+
         })
         .catch(function (error) {
             console.log(error);
         })
 });
+
+const makeImages = (recipes) => {
+    if (recipes.length > 0) {
+        for (let recipe of recipes) {
+            if (recipe.image) {
+                const img = document.createElement('img');
+                img.src = recipe.image;
+                document.body.append(img)
+            }
+        }
+    } else {
+        alert('No Recipes Available With Your Ingredients');
+    }
+}
+
+const removeOldImages = () => {
+    let images = document.querySelectorAll('img');
+    for (var i = 0; i < images.length; i++) {
+        images[i].parentNode.removeChild(images[i]);
+    }
+}
+
+
+
+// const makeImages = (shows) => {
+//     for (let result of shows) {
+//         if (result.show.image) {
+//             const img = document.createElement('img');
+//             img.src = result.show.image.medium;
+//             document.body.append(img)
+//         }
+
+//     }
+// }
+
+
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    // for if we needed just one or so inputs
+    // const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
+    // for if we had multiple types of search inputs
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+    form.elements.query.value = "";
+
+})
 
